009c26934eb6b021a32af0b4226c46ea302a27cd
Oksana <oksana.korobko.fd@gmail.com>
2024-09-11 13:56:02 +0300

    OBB for Polyline ready

--- TaskODA/TaskODA.cpp
+++ TaskODA/TaskODA.cpp
@@ -10,42 +10,6 @@
 
 
 
-//struct Point {
-//    Point() : x(0), y(0) {}
-//    Point(double x_, double y_) : x(x_), y(y_) {}
-//    double x;
-//    double y;
-//};
-//
-//struct Color {
-//    int r;
-//    int g;
-//    int b;
-//
-//    Color(int red, int green, int blue) : r(red), g(green), b(blue) {}
-//};
-
-//class BaseObject {
-//public:
-//    BaseObject() : center(nullptr), color(0, 0, 0) {}
-//    
-//    virtual Point* getCenter() = 0;
-//
-//    void setColor(int r, int g, int b) { color = Color(r, g, b); }
-//
-//    virtual void boundingBox(Point& topLeft, Point& bottomRight) = 0;
-//
-//    virtual void saveToFile(const std::string& filename) = 0;
-//    virtual void loadFromFile(const std::string& filename) = 0;
-//
-//protected:
-//    std::unique_ptr<Point> center;
-//
-//    Color color;
-//};
-
-
-
 class Rect : public BaseObject {
 public:
     Rect() : p1(), p2(), p3(), p4() {}
@@ -181,62 +145,7 @@ private:
     double radius;
 };
 
-//class Triangle : public BaseObject {
-//public:
-//    Triangle() : p1(), p2(), p3() {}
-//
-//    void set(const Point& p1_, const Point& p2_, const Point& p3_) {
-//        p1 = p1_;
-//        p2 = p2_;
-//        p3 = p3_;
-//        center = std::make_unique<Point>((p1.x + p2.x + p3.x) / 3, (p1.y + p2.y + p3.y) / 3);
-//    }
-//
-//    Point* getCenter() override {
-//        return center.get();
-//    }
-//
-//    void boundingBox(Point& topLeft, Point& bottomRight) override {
-//        topLeft.x = std::min({ p1.x, p2.x, p3.x });
-//        topLeft.y = std::max({ p1.y, p2.y, p3.y });
-//        bottomRight.x = std::max({ p1.x, p2.x, p3.x });
-//        bottomRight.y = std::min({ p1.y, p2.y, p3.y });
-//    }
-//
-//    void saveToFile(const std::string& filename) override {
-//        std::ofstream file(filename);
-//        if (file.is_open()) {
-//            file << "Triangle" << std::endl;
-//            file << p1.x << " " << p1.y << std::endl;
-//            file << p2.x << " " << p2.y << std::endl;
-//            file << p3.x << " " << p3.y << std::endl;
-//            file.close();
-//        }
-//    }
-//
-//    void loadFromFile(const std::string& filename) override {
-//        std::ifstream file(filename);
-//        if (!file.is_open()) {
-//            std::cerr << "Unable to open file: " << filename << std::endl;
-//            return;
-//        }
-//        else
-//        {
-//            std::string shapeType;
-//            file >> shapeType;
-//            if (shapeType == "Triangle") {
-//                file >> p1.x >> p1.y;
-//                file >> p2.x >> p2.y;
-//                file >> p3.x >> p3.y;
-//                center = std::make_unique<Point>((p1.x + p2.x + p3.x) / 3, (p1.y + p2.y + p3.y) / 3);
-//            }
-//            file.close();
-//        }
-//    }
-//
-//private:
-//    Point p1, p2, p3;
-//};
+
 
 class Polyline : public BaseObject {
 public:
@@ -290,6 +199,57 @@ public:
     };
 
     void orientedBoundingBox() {
+        Point center;
+
+        for (int i = 0; i < k; i++) {
+            center.x += points[i].x;
+            center.y += points[i].y;
+        }
+        center.x /= k;
+        center.y /= k;
+
+        //Обчислення матриці
+        double covXX, covXY , covYY;
+        covXX =  covXY = covYY = 0.0;
+        for (int i = 0; i < k; i++) {
+            double dx = points[i].x - center.x;
+            double dy = points[i].y - center.y;
+            covXX += dx * dx;
+            covXY += dx * dy;
+            covYY += dy * dy;
+        }
+        covXX /= k;
+        covXY /= k;
+        covYY /= k;
+
+        //Обчислення власних значень
+        double sumCov = covXX + covYY;
+        double determinant = covXX * covYY - covXY * covXY;
+        double l1 = sumCov / 2 + sqrt(sumCov * sumCov / 4 - determinant);
+        double l2 = sumCov / 2 - sqrt(sumCov * sumCov / 4 - determinant);
+
+        // Обчислення напрямків власних векторів
+        double eigVecX = covXY != 0 ? l1 - covYY : 1;
+        double eigVecY = eigVecX != 0 ? covXY / eigVecX : 1;
+        double angle = atan2(eigVecY, eigVecX);
+
+        //Проектування точок на нові осі
+        double minX = 1e9, minY = 1e9, maxX = -1e9, maxY = -1e9;
+        
+        for (int i = 0; i < k; i++) {
+            double rotateX = (points[i].x - center.x) * cos(angle) + (points[i].y - center.y) * sin(angle);
+            double rotateY = -(points[i].x - center.x) * sin(angle) + (points[i].y - center.y) * cos(angle);
+
+            if (rotateX < minX) minX = rotateX;
+            if (rotateX > maxX) maxX = rotateX;
+            if (rotateY < minY) minY = rotateY;
+            if (rotateY > maxY) maxY = rotateY;
+        }
+
+        // Обчислення ширини, висотита центру OBB
+        double width = maxX - minX;
+        double height = maxY - minY;
+        Point obbCenter = { center.x, center.y };
 
     }
 
@@ -374,7 +334,7 @@ int main() {
             Point p3(4.0, 2.0);
             ((Triangle*)pTriangle_1)->set(p1, p2, p3);
             pTriangle_2->set(p1, p2, p3);
-            ((Triangle*)pTriangle_1)-> orientedBoundingBox(p1, p2, p3); //===========================
+          
         }
         
 
